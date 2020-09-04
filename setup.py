703f5f03ce5dcd62b8972fc6efb07691a1df2c18
flo <floriandavid.lannge@web.de>
2020-09-04 11:36:49 +0200

    first content, update requirements and setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='specsnake',
+    version='0.0.0.1',
+    packages=['specsnake'],
+    #url='https://github.com/Darkskald/SFG',
+    license='MIT',
+    author='Darkskald',
+    author_email='floriandavid.lange@web.de',
+    description=''
+)
+
+# todo: install requirements with the help of this file
+# todo: add Github URL as soon as it is pushed
+# todo: Dockerize
