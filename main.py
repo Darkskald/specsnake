43c14623b7a2aaded09513bcec3f946bfd5d15a2
flo <floriandavid.lannge@web.de>
2020-09-04 23:33:17 +0200

    installation command as comment

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# pip install git+https://github.com/Darkskald/specsnake.git@master
